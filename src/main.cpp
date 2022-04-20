f08437fb6fd075409431c89f204326058b25a1aa
Lukas Turcani <lukasturcani@mailbox.org>
2022-04-20 09:03:16 +0100

    Add stuff to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 
 
+auto add_two_numbers(const int a, const int b) -> int
+{
+    return a + b;
+}
+
+
 auto main() -> int
 {
-    std::cout << "Hello, world!" << std::endl;
+    std::cout
+        << "Hello, world! Sum is "
+        << add_two_numbers(1, 2)
+        << std::endl;
     return 0;
 }
